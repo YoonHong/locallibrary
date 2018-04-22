685305e2d900b1a199914e5a1e8fda2f803b5657
Yoon <yoon.s.hong@gmail.com>
2018-04-22 19:59:31 +0900

    Second Commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = config('STATIC_ROOT')
+
+LOGIN_REDIRECT_URL = '/'
