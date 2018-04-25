7846ebc03eb077810313cb83dcef14dcccc32a9c
Yoon <yoon.s.hong@gmail.com>
2018-04-25 22:55:03 +0900

    Email Setting

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = '/static/'
 STATIC_ROOT = config('STATIC_ROOT')
 
 LOGIN_REDIRECT_URL = '/'
+
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'Django Test <noreply@yoonh.xyz>'
+EMAIL_SUBJECT_PREFIX = '[Django Test] '
